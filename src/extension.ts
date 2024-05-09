b5078128ef128f5356c03a827030f3d428c181a7
parnautovic <petararnautovic@gmail.com>
2024-05-09 18:29:20 +0200

    git clone function for git private server

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,8 @@ import simpleGit, { SimpleGit } from 'simple-git';
 
 
 const sshKeyPath = "C:/Users/P53/.ssh/id_rsa";
-
+const username = "raf";
+const password = "masterSI2023";
 // Funkcija za kloniranje repozitorijuma
 // async function cloneRepository() {
 //   // Dobijanje putanje do trenutno otvorenog projekta
@@ -43,12 +44,25 @@ export function activate(context: vscode.ExtensionContext) {
 
       const projectPath = currentWorkspace[0].uri.fsPath;
 
-      // Kloniranje repozitorijuma
-      const git: SimpleGit = simpleGit();
-      await git.clone("C:/Users/Petar/local-git-server/local-git-server", projectPath);
+      try {
+        // Kreiranje instance SimpleGit
+        const git: SimpleGit = simpleGit();
 
-      // Obaveštenje o uspešnom kloniranju
-      vscode.window.showInformationMessage("Repository cloned successfully!");
+        // Postavljanje kredencijala ako su dostupni
+        if (username && password) {
+            git.addConfig('user.name', username);
+            git.addConfig('user.password', password);
+        }
+
+        // Kloniranje repozitorijuma
+        await git.clone('http://raf@192.168.124.28:/java_project.git', projectPath);
+
+        // Obaveštenje o uspešnom kloniranju
+        vscode.window.showInformationMessage("Repository cloned successfully!");
+    } catch (error) {
+        // Prikazivanje greške ako dođe do neuspeha
+        vscode.window.showErrorMessage(`Error cloning repository: ${error}`);
+    }
     }));
 
   context.subscriptions.push(
