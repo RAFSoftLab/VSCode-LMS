cd88e13113ac554cbea020997d044cc1cfb5a3c1
parnautovic <petararnautovic@gmail.com>
2024-05-27 12:57:42 +0200

    postMessage front, comunication with server and list students

--- src/SidebarProvider.ts
+++ src/SidebarProvider.ts
@@ -1,12 +1,13 @@
 import * as vscode from "vscode";
 import { getNonce } from "./getNonce";
+import axios, { type AxiosResponse } from 'axios';
 
 
 export class SidebarProvider implements vscode.WebviewViewProvider {
   _view?: vscode.WebviewView;
   _doc?: vscode.TextDocument;
 
-  constructor(private readonly _extensionUri: vscode.Uri) {}
+  constructor(private readonly _extensionUri: vscode.Uri) { }
 
   public resolveWebviewView(webviewView: vscode.WebviewView) {
     this._view = webviewView;
@@ -26,7 +27,35 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
           if (!data.value) {
             return;
           }
-          vscode.window.showInformationMessage(data.value);
+          try {
+            // Endpoint za dohvat studenata
+            const endpoint = 'http://192.168.124.28:8091/api/v1/students';
+
+            // Token za autorizaciju
+            const API_TOKEN = "L2aTA643Z0UJ43bIdBymFExVbpqZg7v5QJafYh6KFRjl04eV6w4TtdppkX41hEwo";
+
+            // Konfiguracija zahteva sa tokenom
+            const config = {
+              headers: {
+                'Authorization': `Bearer ${API_TOKEN}`
+              }
+            };
+
+            // Izvršavanje GET zahteva sa autorizacijom
+            const response: AxiosResponse = await axios.get(endpoint, config);
+
+            // Provera da li je odgovor uspešan (status 200)
+            if (response.status === 200) {
+              // Prikazivanje rezultata u izlaznom prozoru
+              vscode.window.showInformationMessage(JSON.stringify(response.data));
+            } else {
+              // Ukoliko odgovor nije uspešan, prikaži odgovarajuću poruku
+              throw new Error(`Error fetching students: ${response.statusText}`);
+            }
+          } catch (error: any) {
+            // Prikazivanje greške ako dođe do problema
+            vscode.window.showErrorMessage(`Error fetching students: ${error.message}`);
+          }
           break;
         }
         case "onError": {
@@ -49,15 +78,15 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
       vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
     );
     const styleVSCodeUri = webview.asWebviewUri(
-        vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
-      );
+      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    );
     const scriptUri = webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.js")
     );
     const styleMainUri = webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri, "out", "compiled/sidebar.css")
     );
- 
+
 
     // Use a nonce to only allow a specific script to be run.
     const nonce = getNonce();
@@ -70,13 +99,15 @@ export class SidebarProvider implements vscode.WebviewViewProvider {
 					Use a content security policy to only allow loading images from https or from our extension directory,
 					and only allow scripts that have a specific nonce.
         -->
-        <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${
-      webview.cspSource
-    }; script-src 'nonce-${nonce}';">
+        <meta http-equiv="Content-Security-Policy" content="img-src https: data:; style-src 'unsafe-inline' ${webview.cspSource
+      }; script-src 'nonce-${nonce}';">
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<link href="${styleResetUri}" rel="stylesheet">
 				<link href="${styleVSCodeUri}" rel="stylesheet">
         <link href="${styleMainUri}" rel="stylesheet">
+        <script nonce="${nonce}">
+          const tsvscode = acquireVsCodeApi();
+        </script>
 			</head>
       <body>
 				<script nonce="${nonce}" src="${scriptUri}"></script>
--- src/extension.ts
+++ src/extension.ts
@@ -1,84 +1,103 @@
 import * as vscode from 'vscode';
 import { SidebarProvider } from './SidebarProvider';
 import simpleGit, { SimpleGit } from 'simple-git';
+import axios, {type AxiosResponse}  from 'axios';
 
 
-const sshKeyPath = "C:/Users/P53/.ssh/id_rsa";
 const username = "raf";
 const password = "masterSI2023";
-// Funkcija za kloniranje repozitorijuma
-// async function cloneRepository() {
-//   // Dobijanje putanje do trenutno otvorenog projekta
-//   const currentWorkspace = vscode.workspace.workspaceFolders;
-//   if (!currentWorkspace || !currentWorkspace.length) {
-//       vscode.window.showErrorMessage('No workspace opened.');
-//       return;
-//   }
 
-//   const projectPath = currentWorkspace[0].uri.fsPath;
-
-//   // Kloniranje repozitorijuma
-//   const git: SimpleGit = simpleGit();
-//   await git.clone("C:/Users/Petar/local-git-server/local-git-server", projectPath);
-
-//   // Obaveštenje o uspešnom kloniranju
-//   vscode.window.showInformationMessage("Repository cloned successfully!");
-// }
-
+function getComputerUsername(): string {
+  return vscode.env.machineId; // Koristimo vscode.env.machineName da bismo dobili korisničko ime računara
+}
 export function activate(context: vscode.ExtensionContext) {
-    const sidebarProvider = new SidebarProvider(context.extensionUri);
-        context.subscriptions.push(
-          vscode.window.registerWebviewViewProvider(
-            "vscode-lms-sidebar",
-            sidebarProvider
-          )
-        );
-	  context.subscriptions.push(vscode.commands.registerCommand('vscode-lms.helloWorld', async() => {
-        // Kreiranje i prikazivanje panela
-       // Dobijanje putanje do trenutno otvorenog projekta
-      const currentWorkspace = vscode.workspace.workspaceFolders;
-      if (!currentWorkspace || !currentWorkspace.length) {
-        vscode.window.showErrorMessage('No workspace opened.');
-        return;
+  // Kreiranje i prikazivanje panela
+  const sidebarProvider = new SidebarProvider(context.extensionUri);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      "vscode-lms-sidebar",
+      sidebarProvider
+    )
+  );
+  function getComputerUsername(): string {
+    return vscode.env.machineId; // Koristimo vscode.env.machineName da bismo dobili korisničko ime računara
+}
+  context.subscriptions.push(vscode.commands.registerCommand('vscode-lms.username', async () => {
+    try {
+      // Endpoint za dohvat studenata
+      const endpoint = 'http://192.168.124.28:8091/api/v1/students';
+
+      // Token za autorizaciju
+      const API_TOKEN = "L2aTA643Z0UJ43bIdBymFExVbpqZg7v5QJafYh6KFRjl04eV6w4TtdppkX41hEwo";
+
+      // Konfiguracija zahteva sa tokenom
+      const config = {
+          headers: {
+              'Authorization': `Bearer ${API_TOKEN}`
+          }
+      };
+
+      // Izvršavanje GET zahteva sa autorizacijom
+      const response: AxiosResponse = await axios.get(endpoint, config);
+
+      // Provera da li je odgovor uspešan (status 200)
+      if (response.status === 200) {
+          // Prikazivanje rezultata u izlaznom prozoru
+          vscode.window.showInformationMessage(JSON.stringify(response.data));
+      } else {
+          // Ukoliko odgovor nije uspešan, prikaži odgovarajuću poruku
+          throw new Error(`Error fetching students: ${response.statusText}`);
+      }
+  } catch (error:any) {
+      // Prikazivanje greške ako dođe do problema
+      vscode.window.showErrorMessage(`Error fetching students: ${error.message}`);
+  }
+  }));
+  context.subscriptions.push(vscode.commands.registerCommand('vscode-lms.helloWorld', async () => {
+    // Dobijanje putanje do trenutno otvorenog projekta
+    const currentWorkspace = vscode.workspace.workspaceFolders;
+    if (!currentWorkspace || !currentWorkspace.length) {
+      vscode.window.showErrorMessage('No workspace opened.');
+      return;
+    }
+
+    const projectPath = currentWorkspace[0].uri.fsPath;
+
+    try {
+      // Kreiranje instance SimpleGit
+      const git: SimpleGit = simpleGit();
+
+      // Postavljanje kredencijala ako su dostupni
+      if (username && password) {
+        git.addConfig('user.name', username);
+        git.addConfig('user.password', password);
       }
 
-      const projectPath = currentWorkspace[0].uri.fsPath;
+      // Kloniranje repozitorijuma
+      await git.clone('http://raf@192.168.124.28:/java_project.git', projectPath);
 
-      try {
-        // Kreiranje instance SimpleGit
-        const git: SimpleGit = simpleGit();
-
-        // Postavljanje kredencijala ako su dostupni
-        if (username && password) {
-            git.addConfig('user.name', username);
-            git.addConfig('user.password', password);
-        }
-
-        // Kloniranje repozitorijuma
-        await git.clone('http://raf@192.168.124.28:/java_project.git', projectPath);
-
-        // Obaveštenje o uspešnom kloniranju
-        vscode.window.showInformationMessage("Repository cloned successfully!");
+      // Obaveštenj e o uspešnom kloniranju
+      vscode.window.showInformationMessage("Repository cloned successfully!");
     } catch (error) {
-        // Prikazivanje greške ako dođe do neuspeha
-        vscode.window.showErrorMessage(`Error cloning repository: ${error}`);
+      // Prikazivanje greške ako dođe do neuspeha
+      vscode.window.showErrorMessage(`Error cloning repository: ${error}`);
     }
-    }));
+  }));
 
   context.subscriptions.push(
-      vscode.commands.registerCommand("vscode-lms.refresh", async () => {
-        // HelloWorldPanel.kill();
-        // HelloWorldPanel.createOrShow(context.extensionUri);
-        await vscode.commands.executeCommand("workbench.action.closeSidebar");
-        await vscode.commands.executeCommand(
-          "workbench.view.extension.vscode-lms-sidebar-view"
+    vscode.commands.registerCommand("vscode-lms.refresh", async () => {
+      // HelloWorldPanel.kill();
+      // HelloWorldPanel.createOrShow(context.extensionUri);
+      await vscode.commands.executeCommand("workbench.action.closeSidebar");
+      await vscode.commands.executeCommand(
+        "workbench.view.extension.vscode-lms-sidebar-view"
+      );
+      setTimeout(() => {
+        vscode.commands.executeCommand(
+          "workbench.action.webview.openDeveloperTools"
         );
-        setTimeout(() => {
-          vscode.commands.executeCommand(
-            "workbench.action.webview.openDeveloperTools"
-          );
-        }, 500);
-      })
-    );
+      }, 500);
+    })
+  );
 }
 
